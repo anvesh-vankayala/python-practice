c9c4332a338f068a30b374e1655693ffc3f209fa
anvesh-vankayala <42882800+anvesh-vankayala@users.noreply.github.com>
2024-09-17 14:32:11 +0000

    updated

--- w13/context_manager.py
+++ w13/context_manager.py
@@ -4,6 +4,15 @@ from collections import namedtuple
 
 
 class FileContextIterator:
+    """
+    FileContextIterator :
+    - It is a iterator with, __iter__ and __next__, 
+      which take in csv file path during object creation.
+    - Also it has context manager within to with __enter__ and __exit__, 
+      where 'enter' is triggered during the open of context and 'exit' is triggred during end of context.
+    - It lazyly load data from csv and returns the named tuple dynamically irrespective 
+      of the csv columns number and values.
+    """
 
     def __init__(self,
                  file_path) -> None:
